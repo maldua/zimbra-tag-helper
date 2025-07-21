0de136c364d1ab3fc9f3212473d62557c3474e70
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-07-21 14:15:21 +0200

    zm-build-tags-arguments-from-file.sh: Add pimbra support for extra tags.

--- zm-build-tags-arguments-from-file.sh
+++ zm-build-tags-arguments-from-file.sh
@@ -3,21 +3,54 @@
 #set -x
 #set -v
 
-if (( $# != 2 ))
-then
+usage ()  {
   echo "Usage:   $0 tag pre_filter_file"
   echo "Example: $0 9.0.0.p26 pre_filter_tags_9.0.0.p26.txt"
-  exit 1
-fi
+}
 
 TAG="$1"
 PRE_FILTER_TAGS_FILE="$2"
+PIMBRA_ENABLED="$3" # E.g. 'pimbra-enabled'
 TAG_FOUND="NO"
 
+if [ "x" == "x${TAG}" ] ; then
+  echo "TAG is not defined."
+  usage
+  exit 1
+fi
+
+if [ "x" == "x${PRE_FILTER_TAGS_FILE}" ] ; then
+  echo "pre_filter_file is not defined."
+  usage
+  exit 1
+fi
+
+PIMBRA_TMP_CONFIG_BUILD="pimbra-tmp-config_$$.build"
+
+# Check if Pimbra tag is correct
+MALDUA_PIMBRA_CONFIG_GITHUB_URL="https://github.com/maldua-pimbra/maldua-pimbra-config"
+if [ "pimbra-enabled" == "${PIMBRA_ENABLED}" ] ; then
+  wget "${MALDUA_PIMBRA_CONFIG_GITHUB_URL}"'/raw/refs/tags/'"${TAG}"'/config.build' -O "${PIMBRA_TMP_CONFIG_BUILD}"
+  if [[ $? -ne 0 ]] ; then
+    echo "ERROR: Pimbra config file cannot be downloaded for ${TAG} version !"
+    echo "Aborting !!!"
+    exit 2
+  fi
+fi
+
 ntagCounter=0
+
+if [ "pimbra-enabled" == "${PIMBRA_ENABLED}" ] ; then
+  echo -e -n "${TAG}"
+  let ntagCounter=$ntagCounter+1
+  TAG_TO_FIND="${TAG%.[pP]*}"
+else
+  TAG_TO_FIND="${TAG}"
+fi
+
 for ntag in $(cat ${PRE_FILTER_TAGS_FILE}); do
 
-  if [[ "${TAG}" == "${ntag}" ]] ; then
+  if [[ "${TAG_TO_FIND}" == "${ntag}" ]] ; then
     TAG_FOUND="YES"
   fi
 
@@ -30,3 +63,8 @@ for ntag in $(cat ${PRE_FILTER_TAGS_FILE}); do
   fi
 
 done
+
+# Remove tmp files
+if [ "pimbra-enabled" == "${PIMBRA_ENABLED}" ] ; then
+  rm ${PIMBRA_TMP_CONFIG_BUILD}
+fi
