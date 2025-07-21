7db3b203bbbaf9111699f43a9c71b8970fa9db1e
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-07-21 14:18:49 +0200

    zm-build-tags-arguments.sh: Add pimbra support for extra tags.

--- zm-build-tags-arguments.sh
+++ zm-build-tags-arguments.sh
@@ -3,18 +3,27 @@
 #set -x
 #set -v
 
-if (( $# != 1 ))
-then
+usage () {
   echo "Usage:   $0 tag"
   echo "Example: $0 9.0.0.p26"
+
+  echo "Usage:   $0 tag pimbra-enabled"
+  echo "Example: $0 9.0.0.p26 pimbra-enabled"
+}
+
+TAG="$1" # E.g. 10.0.7p0
+PIMBRA_ENABLED="$2" # E.g. 'pimbra-enabled'
+
+if [ "x" == "x${TAG}" ] ; then
+  echo "TAG is not defined."
+  usage
   exit 1
 fi
 
-TAG="$1"
 PRE_FILTER_TAGS_FILE="$$-pre-filter-tags-file.txt"
 TAG_FOUND="NO"
 
 ./zm-build-filter-tags.sh ${TAG} > ${PRE_FILTER_TAGS_FILE}
-./zm-build-tags-arguments-from-file.sh ${TAG} ${PRE_FILTER_TAGS_FILE}
+./zm-build-tags-arguments-from-file.sh ${TAG} ${PRE_FILTER_TAGS_FILE} ${PIMBRA_ENABLED}
 
 rm ${PRE_FILTER_TAGS_FILE}
